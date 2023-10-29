0ee3175ad6d4f59e14dd58d5a49a92d6dd8d0f14
KymaiselHunter <kyle.bautista78@myhunter.cuny.edu>
2023-10-29 17:07:29 -0400

    New issue spotted, in my quest query, i am sometimes printing the ptr itself instead of the quest title, must fix that along with the fact that it's missing some not ready quests

--- main.cpp
+++ main.cpp
@@ -25,11 +25,33 @@ int main()
     questList.questQuery("Quest 1");
     std::cout<<std::endl;
 
+    questList.questQuery("Quest 2");
+    std::cout<<std::endl;
+
+    questList.questQuery("Quest 3");
+    std::cout<<std::endl;
+
+    questList.questQuery("Quest 4");
+    std::cout<<std::endl;
+
+    questList.questQuery("Quest 5");
+    std::cout<<std::endl;
+
+    questList.questQuery("Quest 6");
+    std::cout<<std::endl;
+
+    questList.questQuery("Quest 7");
+    std::cout<<std::endl;
+
+    questList.questQuery("Quest 8");
+    std::cout<<std::endl;
+
+    questList.questQuery("Quest 9");
+    std::cout<<std::endl;
+
     std::cout << "Testing printQuestDetails:" << std::endl<< std::endl;
     Quest* current = questList.getPointerTo(questList.getPosOf("Quest 1"))->getItem();
     questList.printQuestDetails(current);
     std::cout<<std::endl;
 
-    cout << " dsf  " << questList.calculatePathwayExperience(current) << endl;
-    cout << " dsf2  " << questList.calculateProjectedExperience(current) << endl;
 }
