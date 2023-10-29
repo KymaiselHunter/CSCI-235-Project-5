4457e01c087a59182d9e55d7deea11d749477427
KymaiselHunter <kyle.bautista78@myhunter.cuny.edu>
2023-10-29 17:00:12 -0400

    tested and now saving as i will make changes from here on in the revamps branch, the current issue i have found that pathway XP should also include the quest given in the paremeters

--- main.cpp
+++ main.cpp
@@ -13,8 +13,23 @@
 using namespace std;
 
 #include "DoublyLinkedList.hpp"
+#include "QuestList.hpp"
 
 int main()
 {
-    cout << "Test 1" << endl;
+    //cout << "Test 1" << endl;
+
+    QuestList questList = QuestList("debug.csv");
+
+    std::cout << "Testing questQuery:" << std::endl<< std::endl;
+    questList.questQuery("Quest 1");
+    std::cout<<std::endl;
+
+    std::cout << "Testing printQuestDetails:" << std::endl<< std::endl;
+    Quest* current = questList.getPointerTo(questList.getPosOf("Quest 1"))->getItem();
+    questList.printQuestDetails(current);
+    std::cout<<std::endl;
+
+    cout << " dsf  " << questList.calculatePathwayExperience(current) << endl;
+    cout << " dsf2  " << questList.calculateProjectedExperience(current) << endl;
 }
