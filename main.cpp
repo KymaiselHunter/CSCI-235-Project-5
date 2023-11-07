85e8f95f4c890048b6f16ee02a7de1c20ef84f48
KymaiselHunter <kyle.bautista78@myhunter.cuny.edu>
2023-11-06 19:55:03 -0500

    just some tests

--- main.cpp
+++ main.cpp
@@ -15,144 +15,150 @@ using namespace std;
 #include "DoublyLinkedList.hpp"
 #include "QuestList.hpp"
 
-int main()
-{
-    //cout << "Test 1" << endl;
+// int main()
+// {
+//     //cout << "Test 1" << endl;
 
-    QuestList questList = QuestList("debug.csv");
+//     QuestList questList = QuestList("debug.csv");
 
-    std::cout << "Testing questQuery:" << std::endl<< std::endl;
-    questList.questQuery("Quest 1");
-    std::cout<<std::endl;
+//     std::cout << "Testing questQuery:" << std::endl<< std::endl;
+//     questList.questQuery("Quest 1");
+//     std::cout<<std::endl;
 
-    questList.questQuery("Quest 2");
-    std::cout<<std::endl;
+//     questList.questQuery("Quest 2");
+//     std::cout<<std::endl;
 
-    questList.questQuery("Quest 3");
-    std::cout<<std::endl;
+//     questList.questQuery("Quest 3");
+//     std::cout<<std::endl;
 
-    questList.questQuery("Quest 4");
-    std::cout<<std::endl;
+//     questList.questQuery("Quest 4");
+//     std::cout<<std::endl;
 
-    //questList.printQuest(questList.getPointerTo(questList.getPosOf("Quest 1"))->getItem());
+//     //questList.printQuest(questList.getPointerTo(questList.getPosOf("Quest 1"))->getItem());
 
-    questList.questQuery("Quest 5");
-    std::cout<<std::endl;
+//     questList.questQuery("Quest 5");
+//     std::cout<<std::endl;
 
-    questList.questQuery("Quest 6");
-    std::cout<<std::endl;
+//     questList.questQuery("Quest 6");
+//     std::cout<<std::endl;
 
-    questList.questQuery("Quest 7");
-    std::cout<<std::endl;
+//     questList.questQuery("Quest 7");
+//     std::cout<<std::endl;
 
-    questList.questQuery("Quest 8");
-    std::cout<<std::endl;
+//     questList.questQuery("Quest 8");
+//     std::cout<<std::endl;
 
-    questList.questQuery("Quest 9");
-    std::cout<<std::endl;
+//     questList.questQuery("Quest 9");
+//     std::cout<<std::endl;
 
-    questList.questQuery("Quest 12");
-    std::cout<<std::endl;
+//     questList.questQuery("Quest 12");
+//     std::cout<<std::endl;
 
-    std::cout << "Testing printQuestDetails:" << std::endl<< std::endl;
-    Quest* current = questList.getPointerTo(questList.getPosOf("Quest 1"))->getItem();
-    questList.printQuestDetails(current);
-    std::cout<<std::endl;
+//     std::cout << "Testing printQuestDetails:" << std::endl<< std::endl;
+//     Quest* current = questList.getPointerTo(questList.getPosOf("Quest 1"))->getItem();
+//     questList.printQuestDetails(current);
+//     std::cout<<std::endl;
     
-    current = questList.getPointerTo(questList.getPosOf("Quest 2"))->getItem();
-    questList.printQuestDetails(current);
-    std::cout<<std::endl;
+//     current = questList.getPointerTo(questList.getPosOf("Quest 2"))->getItem();
+//     questList.printQuestDetails(current);
+//     std::cout<<std::endl;
 
-    current = questList.getPointerTo(questList.getPosOf("Quest 3"))->getItem();
-    questList.printQuestDetails(current);
-    std::cout<<std::endl;
-
-
-    //questList.KyleDisplay();
-
-    //questList.calculatePathwayExperience(current);
-
-    // std::cout << current->title_ << " dependents:" << endl;
-    // for(int i = 0; i < current->subquests_.size(); i++)
-    // {
-    //     cout << current->subquests_[i] << endl;
-    // }
-
-    current = questList.getPointerTo(questList.getPosOf("Quest 4"))->getItem();
-    questList.printQuestDetails(current);
-    std::cout<<std::endl;
-
-    current = questList.getPointerTo(questList.getPosOf("Quest 5"))->getItem();
-    questList.printQuestDetails(current);
-    std::cout<<std::endl;
-
-    current = questList.getPointerTo(questList.getPosOf("Quest 6"))->getItem();
-    questList.printQuestDetails(current);
-    std::cout<<std::endl;
-
-    current = questList.getPointerTo(questList.getPosOf("Quest 7"))->getItem();
-    questList.printQuestDetails(current);
-    std::cout<<std::endl;
-
-    current = questList.getPointerTo(questList.getPosOf("Quest 8"))->getItem();
-    questList.printQuestDetails(current);
-    std::cout<<std::endl;
-
-    current = questList.getPointerTo(questList.getPosOf("Quest 9"))->getItem();
-    questList.printQuestDetails(current);
-    std::cout<<std::endl;
-
-    // //lets also test functions that we are not given to test
-    // questList.questHistory();
-    // cout << "Total gained Experience: " << questList.calculateGainedExperience() << endl;
-
-    //lets test quest history
-    std::cout << "===================================" << std::endl;
-    std::cout << "Testing quest History" << std::endl;
-    std::cout << "===================================" << std::endl;
-
-    std::cout << "===================================" << std::endl;
-    std::cout << "No filter" << std::endl;
-    std::cout << "===================================" << std::endl;
-    questList.questHistory();//none
-
-    std::cout << "===================================" << std::endl;
-    std::cout << "COMPLETE" << std::endl;
-    std::cout << "===================================" << std::endl;
-
-    questList.questHistory("COMPLETE");//complete
+//     current = questList.getPointerTo(questList.getPosOf("Quest 3"))->getItem();
+//     questList.printQuestDetails(current);
+//     std::cout<<std::endl;
 
 
-    std::cout << "===================================" << std::endl;
-    std::cout << "INCOMPLETE" << std::endl;
-    std::cout << "===================================" << std::endl;
+//     //questList.KyleDisplay();
 
-    questList.questHistory("INCOMPLETE");
+//     //questList.calculatePathwayExperience(current);
+
+//     // std::cout << current->title_ << " dependents:" << endl;
+//     // for(int i = 0; i < current->subquests_.size(); i++)
+//     // {
+//     //     cout << current->subquests_[i] << endl;
+//     // }
+
+//     current = questList.getPointerTo(questList.getPosOf("Quest 4"))->getItem();
+//     questList.printQuestDetails(current);
+//     std::cout<<std::endl;
+
+//     current = questList.getPointerTo(questList.getPosOf("Quest 5"))->getItem();
+//     questList.printQuestDetails(current);
+//     std::cout<<std::endl;
+
+//     current = questList.getPointerTo(questList.getPosOf("Quest 6"))->getItem();
+//     questList.printQuestDetails(current);
+//     std::cout<<std::endl;
+
+//     current = questList.getPointerTo(questList.getPosOf("Quest 7"))->getItem();
+//     questList.printQuestDetails(current);
+//     std::cout<<std::endl;
+
+//     current = questList.getPointerTo(questList.getPosOf("Quest 8"))->getItem();
+//     questList.printQuestDetails(current);
+//     std::cout<<std::endl;
+
+//     current = questList.getPointerTo(questList.getPosOf("Quest 9"))->getItem();
+//     questList.printQuestDetails(current);
+//     std::cout<<std::endl;
+
+//     // //lets also test functions that we are not given to test
+//     // questList.questHistory();
+//     // cout << "Total gained Experience: " << questList.calculateGainedExperience() << endl;
+
+//     //lets test quest history
+//     std::cout << "===================================" << std::endl;
+//     std::cout << "Testing quest History" << std::endl;
+//     std::cout << "===================================" << std::endl;
+
+//     std::cout << "===================================" << std::endl;
+//     std::cout << "No filter" << std::endl;
+//     std::cout << "===================================" << std::endl;
+//     questList.questHistory();//none
+
+//     std::cout << "===================================" << std::endl;
+//     std::cout << "COMPLETE" << std::endl;
+//     std::cout << "===================================" << std::endl;
+
+//     questList.questHistory("COMPLETE");//complete
+
+
+//     std::cout << "===================================" << std::endl;
+//     std::cout << "INCOMPLETE" << std::endl;
+//     std::cout << "===================================" << std::endl;
+
+//     questList.questHistory("INCOMPLETE");
     
-    std::cout << "===================================" << std::endl;
-    std::cout << "AVAILABLE" << std::endl;
-    std::cout << "===================================" << std::endl;
-    questList.questHistory("AVAILABLE");
+//     std::cout << "===================================" << std::endl;
+//     std::cout << "AVAILABLE" << std::endl;
+//     std::cout << "===================================" << std::endl;
+//     questList.questHistory("AVAILABLE");
 
-    std::cout << "===================================" << std::endl;
-    std::cout << "Invalid filter" << std::endl;
-    std::cout << "===================================" << std::endl;
-    questList.questHistory("rahhhhh");
+//     std::cout << "===================================" << std::endl;
+//     std::cout << "Invalid filter" << std::endl;
+//     std::cout << "===================================" << std::endl;
+//     questList.questHistory("rahhhhh");
 
 
-    std::cout << "===================================" << std::endl;
-    std::cout << "Kyle's Display" << std::endl;
-    std::cout << "===================================" << std::endl;
+//     std::cout << "===================================" << std::endl;
+//     std::cout << "Kyle's Display" << std::endl;
+//     std::cout << "===================================" << std::endl;
 
-    questList.KyleFullDisplay();
+//     questList.KyleDisplay();
 
-    // string description = questList.getPointerTo(questList.getPosOf("Quest 9"))->getItem()->description_;
+//     std::cout << "===================================" << std::endl;
+//     std::cout << "Kyle's Full Display" << std::endl;
+//     std::cout << "===================================" << std::endl;
 
-    // for(int i = 0; i < description.size(); i++)
-    // {
-    //     cout << i << " "<< description[i] << endl;
-    // }
-}
+//     questList.KyleFullDisplay();
+
+//     // string description = questList.getPointerTo(questList.getPosOf("Quest 9"))->getItem()->description_;
+
+//     // for(int i = 0; i < description.size(); i++)
+//     // {
+//     //     cout << i << " "<< description[i] << endl;
+//     // }
+// }
 
 // int main()
 // {
@@ -262,3 +268,22 @@ int main()
 //     // questList.printQuestDetails(current);
 //     // std::cout<<std::endl;
 // }
+
+int main()
+{
+    
+    QuestList questList = QuestList("quests.csv");
+
+    std::cout << "===================================" << std::endl;
+    std::cout << "Kyle's Display" << std::endl;
+    std::cout << "===================================" << std::endl;
+
+    questList.KyleDisplay();
+
+    std::cout << "===================================" << std::endl;
+    std::cout << "Kyle's Full Display" << std::endl;
+    std::cout << "===================================" << std::endl;
+
+    questList.KyleFullDisplay();
+
+}
